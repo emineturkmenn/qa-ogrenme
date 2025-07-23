77e6852c4d324367b457ecc04d86ca181be13a4e
Emine Türkmen <oemine075@gmail.com>
2025-07-24 00:16:39 +0300

    toplama fonksiyonu ve ilk pytest testi eklendi

--- test_math_operations.py
+++ test_math_operations.py
@@ -1,11 +1,6 @@
-def topla(a,b):
-    return a+b
+from math_operations import topla
 
 def test_topla():
-    assert topla(2,3) == 5
+    assert topla(3,5) == 8
     assert topla(-1,1) == 0
     assert topla(0,0) == 0
-    # assert topla(2, 3) == 6, "Toplama hatalı sonuç verdi"
-
-test_topla()
-print("Tüm testler geçti ✅")
