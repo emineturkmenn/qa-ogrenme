85770b2e59782ce6e5deae380aec5b0204e30703
Emine Türkmen <oemine075@gmail.com>
2025-08-12 23:39:14 +0300

    fix: conftest.py dosyasını sadeleştirip user-data-dir sorununu çözdüm

--- conftest.py
+++ conftest.py
@@ -5,17 +5,24 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 
-def pytest_addoption(parser):
-    parser.addoption(
-        "--headless", action="store_true", help="Headless modda çalıştır"
-    )
-
 @pytest.fixture
-def browser():
+def browser(request):
     options = ChromeOptions()
+
+    headless = request.config.getoption("--headless") if hasattr(request.config, 'getoption') else False
+    if headless:
+        options.add_argument("--headless=new")
+
     options.add_argument("--window-size=1920,1080")
+
     service = ChromeService(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
     driver.wait = WebDriverWait(driver, 10)
+
     yield driver
     driver.quit()
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--headless", action="store_true", default=False, help="Headless modda çalıştır"
+    )
