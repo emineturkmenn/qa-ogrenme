d68b8ed8aa3d65c92f0e6bb880473b01bb36a2b8
Emine Türkmen <oemine075@gmail.com>
2025-08-12 23:35:35 +0300

    düzeltme: Sadece chrome da çalışacak şekilde ayarlandı

--- conftest.py
+++ conftest.py
@@ -1,53 +1,21 @@
 import pytest
-import shutil
-import tempfile
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 
 def pytest_addoption(parser):
-    parser.addoption(
-        "--browser", action="store", default="chrome", help="Tarayıcı adı: chrome veya firefox"
-    )
     parser.addoption(
         "--headless", action="store_true", help="Headless modda çalıştır"
     )
 
 @pytest.fixture
-def browser(request):
-    browser_name = request.config.getoption("--browser")
-    headless = request.config.getoption("--headless")
-
-    if browser_name == "chrome":
-        options = ChromeOptions()
-        if headless:
-            options.add_argument("--headless=new")
-        options.add_argument("--window-size=1920,1080")
-        user_data_dir = tempfile.mkdtemp()
-        options.add_argument(f"--user-data-dir={user_data_dir}")
-        service = ChromeService(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service, options=options)
-
-
-    elif browser_name == "firefox":
-        if not shutil.which("firefox"):
-            pytest.skip("Firefox yüklü değil, test atlandı.")
-        options = FirefoxOptions()
-        if headless:
-            options.add_argument("--headless")
-        options.add_argument("--width=1920")
-        options.add_argument("--height=1080")
-        service = FirefoxService(GeckoDriverManager().install())
-        driver = webdriver.Firefox(service=service, options=options)
-
-    else:
-        raise ValueError(f"Geçersiz tarayıcı seçimi: {browser_name}")
-
+def browser():
+    options = ChromeOptions()
+    options.add_argument("--window-size=1920,1080")
+    service = ChromeService(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
     driver.wait = WebDriverWait(driver, 10)
     yield driver
     driver.quit()
